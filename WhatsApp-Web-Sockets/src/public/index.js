88aa0f5520a8d24a6d61672c03409e7d6401c5ce
Esteban <estebanhirzfeld@gmail.com>
2022-04-01 17:36:07 -0300

    Fix: HTML tags are no longer allowed in userName

--- WhatsApp-Web-Sockets/src/public/index.js
+++ WhatsApp-Web-Sockets/src/public/index.js
@@ -15,7 +15,7 @@ var elem = document.getElementById("message-container");
 
 // Text Image Url 
 const getInputMessage = () => {
-    chatBox.value = chatBox.value.replace("<", '');
+    chatBox.value = chatBox.value.replace("<", '').replace(">", '');
     // if input starts with http:// or https://, open in new tab
     if (chatBox.value.trim().endsWith(".jpg") || chatBox.value.trim().endsWith(".png") || chatBox.value.trim().endsWith(".gif") || chatBox.value.trim().endsWith(".jpeg")) {
         socket.emit("message", {
@@ -117,6 +117,7 @@ Swal.fire(
         }
     }
 ).then((result) => {
+    result.value = result.value.replace("<", '').replace(">", '');
     user = result.value;
 });
 
